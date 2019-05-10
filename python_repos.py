d4d7c92febbd18483cb0c9f7c16be9a534d699b7
2shsh <kashshapov@gmail.com>
2019-05-10 18:07:18 +0500

    Print information about most popular project

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories' \
@@ -15,19 +17,18 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# Анализ первого репозитория.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-      print(key)
+names, stars = [], []
+for repo_dict in repo_dicts:
+      names.append(repo_dict['name'])
+      stars.append(repo_dict['stargazers_count'])
 
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+# Построение визуализации.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45,
+                  show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
 
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
 
